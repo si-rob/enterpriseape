d6b1ef0904b0e3140e179f5c37b014f0eb91e145
Simon Roberts <simon.roberts@life.church>
2018-12-01 10:59:58 -0600

    add csv importer;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_202623) do
+ActiveRecord::Schema.define(version: 2018_12_01_161230) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
