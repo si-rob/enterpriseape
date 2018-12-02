820ab04e4e3cf7d27f37b39421f8bfd2f546b630
Simon Roberts <simon.roberts@life.church>
2018-12-01 22:13:39 -0600

    add invoice status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_161230) do
+ActiveRecord::Schema.define(version: 2018_12_02_040419) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_12_01_161230) do
     t.string "salesperson"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status_type"
   end
 
   create_table "users", force: :cascade do |t|
